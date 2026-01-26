ee6a05d319d98d3551a105cf1e04cfc6f2fbffc9
techentia-work <prabhjotwork2004@gmail.com>
2026-01-26 23:22:30 +0530

    final admin changes v7

--- server/src/controller/emailController/index.js
+++ server/src/controller/emailController/index.js
@@ -4,7 +4,7 @@ const { generateInvoicePDF } = require("../../utils/invoiceGenerator");
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: process.env.SMTP_SECURE === "true", // Convert string to boolean
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
